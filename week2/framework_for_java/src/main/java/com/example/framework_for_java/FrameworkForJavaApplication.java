68e70e2520f7c4851e1e88aa57faa946d47c2451
SeungHan <jk05018@naver.com>
2022-03-23 15:56:48 +0900

    week2

---
+++ week2/framework_for_java/src/main/java/com/example/framework_for_java/FrameworkForJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.framework_for_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkForJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkForJavaApplication.class, args);
+	}
+
+}
---
+++ week2/framework_for_java/src/test/java/com/example/framework_for_java/FrameworkForJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.framework_for_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkForJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
