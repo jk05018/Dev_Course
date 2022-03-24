242e17300c0b693548d4ef78fbf638fc0a3f003d
SeungHan <jk05018@naver.com>
2022-03-24 17:36:16 +0900

    MyIterator 구현

--- week2/framework_for_java/src/main/java/com/example/framework_for_java/collections/MyCollection.java
+++ week2/framework_for_java/src/main/java/com/example/framework_for_java/collections/MyCollection.java
@@ -6,6 +6,8 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import com.example.framework_for_java.iter.MyIterator;
+
 public class MyCollection<T> {
 	private List<T> list;
 
@@ -41,5 +43,21 @@ public class MyCollection<T> {
 			T data = list.get(i);
 			consumer.accept(data);
 		}
+	}
+
+	public MyIterator<T> iterator(){
+		return new MyIterator<T>() {
+			private int index;
+
+			@Override
+			public boolean hasNext() {
+				return index < list.size();
+			}
+
+			@Override
+			public T next() {
+				return list.get(index++);
+			}
+		};
 	}
 }
---
+++ week2/framework_for_java/src/main/java/com/example/framework_for_java/iter/MyIterator.java
@@ -0,0 +1,7 @@
+package com.example.framework_for_java.iter;
+
+public interface MyIterator<T> {
+	boolean hasNext();
+
+	T next();
+}
